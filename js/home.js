1c3ae47958a9fa925ae7f2b6c63a1a746ef4bc65
siamkhan7426 <sieam.khan12@gmail.com>
2024-12-03 20:39:48 +0600

    amount upadate function add

--- js/home.js
+++ js/home.js
@@ -1,21 +1,42 @@
 // step - 1 event kaj korta gela button ta key agie dorbo tar vitor kaj korbo bakai sob
 // step - 2 preventDefault madddoma page relaod problem solve korbo
 // step - 3 input gulo dora ney asa 
-// step - 4 validation korbo if()
+// step - 4 validation korbo if()--- are vitor sob kisu korta hoba jodi true hoi sob kaj hoba 
+// step - 5 amr total balance
+// step - 6 amr total balace sata input theka jay amount asba + kora dibo
+// step - 7 update total balance 
 
-const butotnAddMoney = document.getElementById('btn-money')
-butotnAddMoney.addEventListener('click', function (event){
+    const butotnAddMoney = document.getElementById('btn-money')
+    butotnAddMoney.addEventListener('click', function (event){
     // web page realod problem
     event.preventDefault()
     //console.log("hello money")
     // 
-    const inputAddAmount = document.getElementById('add-amount').value = "";
-    const inputYourPin = document.getElementById('your-pin').value = "";
-    console.log(inputAddAmount, inputYourPin)
+    const inputAddAmount = document.getElementById('add-amount')
+    const inputAddAmountValue = inputAddAmount.value;
+    const inputAddAmountValueNumber = Number(inputAddAmountValue);
+    inputAddAmount.value = "";
+    const inputYourPin = document.getElementById('your-pin')
+    const inputYourPinValue = inputYourPin.value;
+    inputYourPin.value = "";
 
-    if(inputYourPin === '1234'){
+    //console.log(inputAddAmountValueNumber, inputYourPinValue)
+
+    if(inputYourPinValue === "1234"){
+        // step -5 total current balance
+        let accountMoney = document.getElementById('account-balance').innerText;
+        let moneyBalance = Number(accountMoney);
+        //console.log(typeof moneyBalance, moneyBalance);
+
+        // step - 6  input amount + total balance
+        let newBalance = inputAddAmountValueNumber  + moneyBalance;
+        //console.log(newBalance)
+
+        // step - 7 total amount balance
+        document.getElementById('account-balance').innerText = newBalance;
+        // alert messege
         alert("successful your money add")
     }else{
-        alert('wrong.!')
+        alert('Money not add sorry! Please try againe. ')
     }
 })
